be15fb6677a6e7cbc9138db19125e178ff5e95f5
Parker DeWitt <parker@drakehomesllc.com>
2025-06-25 17:13:04 -0500

    Update map layers to use cleaner CartoDB Light tiles for better visual clarity

--- components/property-map.tsx
+++ components/property-map.tsx
@@ -104,8 +104,8 @@ export default function PropertyMap({ location, title, price, latitude, longitud
         ref={mapRef}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         <Marker position={coordinates}>
           <Popup>
